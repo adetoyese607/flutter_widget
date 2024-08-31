e85f880a895b8a7b3aa35926df1c976e69525b11
Matthew <adetoyesesamson@gmail.com>
2024-08-31 23:14:38 +0100

    Widgetpadding

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -38,29 +39,77 @@ class _MyHomePageState extends State<MyHomePage> {
       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       title: Text(widget.title),
     ),
-    body: Center(
-      child:  Text.rich(
-        style: TextStyle(
-           fontSize: 20,
-                background: Paint()
-                  ..color = Colors.blue
-                  ..strokeWidth = 2
-                  ..style = PaintingStyle.stroke
-        ),
-    const    TextSpan(
-            text: 'I ',
-           
-            children: [
-              WidgetSpan(
-                  child: Icon(
-                Icons.favorite_outline,
-                color: Colors.blue,
-              )),
-              TextSpan(
-                text: ' Flutter',
+  body: Center(
+    
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      //   child:  Text.rich(
+      //     style: TextStyle(
+      //        fontSize: 20,
+      //             background: Paint()
+      //               ..color = Colors.blue
+      //               ..strokeWidth = 2
+      //               ..style = PaintingStyle.stroke
+      //     ),
+      // const    TextSpan(
+      //         text: 'I ',
+             
+      //         children: [
+      //           WidgetSpan(
+      //               child: Icon(
+      //             Icons.favorite_outline,
+      //             color: Colors.blue,
+      //           )),
+      //           TextSpan(
+      //             text: ' Flutter',
+      //           )
+      //         ]
+      //         )
+      //         ),
+      
+       children:<Widget> [
+         RichText(
+        text:const TextSpan(
+          text: 'Balance\n',
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.black87,
+          ),
+          children: [
+            TextSpan(
+              text: '£1,23,453',
+              style: TextStyle(
+                color: Colors.blue, fontSize: 30,
               )
-            ])),
-    ),
+            )
+          ]
+        ),
+        ),
+      Padding(padding:const EdgeInsets.only(top: 50),
+
+     child: RichText(
+      text: TextSpan(
+        text: 'Don\'t have an account?',
+        style:const TextStyle(
+          fontSize: 20,
+          color: Colors.black87,
+        ),
+        children: [
+          TextSpan(
+            text: 'Register',
+            style:const TextStyle(
+              color: Colors.orange, fontWeight: FontWeight.w500,
+            ),
+            recognizer: TapGestureRecognizer()..onTap = (){}
+          )
+        ]
+      ),
+      ),
+     )
+       ]
+      ),
+  ),
+    
     );
   }
 }
