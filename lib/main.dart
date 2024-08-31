feaf18ecd2ec9bf4b227817d9b43c5682ea76615
Matthew <adetoyesesamson@gmail.com>
2024-08-31 23:56:45 +0100

    Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
@@ -35,81 +37,97 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      title: Text(widget.title),
-    ),
-  body: Center(
-    
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      //   child:  Text.rich(
-      //     style: TextStyle(
-      //        fontSize: 20,
-      //             background: Paint()
-      //               ..color = Colors.blue
-      //               ..strokeWidth = 2
-      //               ..style = PaintingStyle.stroke
-      //     ),
-      // const    TextSpan(
-      //         text: 'I ',
-             
-      //         children: [
-      //           WidgetSpan(
-      //               child: Icon(
-      //             Icons.favorite_outline,
-      //             color: Colors.blue,
-      //           )),
-      //           TextSpan(
-      //             text: ' Flutter',
-      //           )
-      //         ]
-      //         )
-      //         ),
-      
-       children:<Widget> [
-         RichText(
-        text:const TextSpan(
-          text: 'Balance\n',
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.black87,
-          ),
-          children: [
-            TextSpan(
-              text: '£1,23,453',
-              style: TextStyle(
-                color: Colors.blue, fontSize: 30,
-              )
-            )
-          ]
-        ),
-        ),
-      Padding(padding:const EdgeInsets.only(top: 50),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            //   child:  Text.rich(
+            //     style: TextStyle(
+            //        fontSize: 20,
+            //             background: Paint()
+            //               ..color = Colors.blue
+            //               ..strokeWidth = 2
+            //               ..style = PaintingStyle.stroke
+            //     ),
+            // const    TextSpan(
+            //         text: 'I ',
 
-     child: RichText(
-      text: TextSpan(
-        text: 'Don\'t have an account?',
-        style:const TextStyle(
-          fontSize: 20,
-          color: Colors.black87,
-        ),
-        children: [
-          TextSpan(
-            text: 'Register',
-            style:const TextStyle(
-              color: Colors.orange, fontWeight: FontWeight.w500,
+            //         children: [
+            //           WidgetSpan(
+            //               child: Icon(
+            //             Icons.favorite_outline,
+            //             color: Colors.blue,
+            //           )),
+            //           TextSpan(
+            //             text: ' Flutter',
+            //           )
+            //         ]
+            //         )
+            //         ),
+
+            children: <Widget>[
+              RichText(
+                text: const TextSpan(
+                    text: 'Balance\n',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black87,
+                    ),
+                    children: [
+                      TextSpan(
+                          text: '£1,23,453',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 30,
+                          ))
+                    ]),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 50),
+                child: RichText(
+                  text: TextSpan(
+                      text: 'Don\'t have an account?',
+                      style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.black87,
+                      ),
+                      children: [
+                        TextSpan(
+                            text: ' Register',
+                            style: const TextStyle(
+                              color: Colors.orange,
+                              fontWeight: FontWeight.w500,
+                            ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                // ignore: avoid_print
+                                print('Lets get started');
+                              })
+                      ]),
+                ),
+              ),
+              Padding(padding:const EdgeInsets.only(top: 30),
+             child: RichText(
+              text:const TextSpan(
+                text: 'Please Subscribe ', style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black87,
+
+                ),
+                children: [
+                  WidgetSpan(child: Icon( Icons.notifications_active_outlined, size: 30, color: Colors.blue),
+                  alignment:PlaceholderAlignment.middle
+                  )
+                ]
+              ),
+              ),
+             )
+            ]
             ),
-            recognizer: TapGestureRecognizer()..onTap = (){}
-          )
-        ]
       ),
-      ),
-     )
-       ]
-      ),
-  ),
-    
     );
   }
 }
